125ca412c51a1c9b5b28d6bc8df90156e049011f
Kevin Uyttenhove <kevin.uyttenhove@student.howest.be>
2019-05-12 22:24:33 +0200

    Unittests

--- Spinning/Spinning.UnitTests/APITests.cs
+++ Spinning/Spinning.UnitTests/APITests.cs
@@ -1,15 +1,29 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using Spinning.API.Controllers;
+using Spinning.API.DTOs;
+using Spinning.Models;
 using Spinning.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Spinning.UnitTests
 {
     [TestClass]
     public class APITests
     {
-        
+        //private Task<List<Timeslot>> timeslot;
+        //[TestMethod]
+        //private async Task GetTimeSlot_Returns_Value()
+        //{
+        //    var mockTimeSlot = new Mock<ITimeSlotRepository>();
+        //    mockTimeSlot.Setup(t => t.GetAllAsync()).Returns(timeslot);
+
+        //    var controller = new TimeslotsController(mockTimeSlot.Object);
+
+        //}
     }
 }
--- Spinning/Spinning.UnitTests/WebAppTests.cs
+++ Spinning/Spinning.UnitTests/WebAppTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Spinning.Models;
 using Spinning.Models.Repositories;
@@ -10,11 +12,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
+
 namespace Spinning.UnitTests
 {
+    
     [TestClass] public class WebAppTests
     {
+        
         private Task<List<Timeslot>> timeslotList = Task.FromResult(new List<Timeslot>());
+        private Task<Timeslot> timeslot = Task.FromResult(new Timeslot());
 
         private async Task<Timeslot> GetTimeslot(int id = 3)
         {
@@ -27,19 +33,41 @@ namespace Spinning.UnitTests
         {
             //arrange
             var mocktimeslotRepo = new Mock<ITimeSlotRepository>();
-            mocktimeslotRepo.Setup(r => r.GetByIdAsync(3)).Returns(GetTimeslot(3));
+            mocktimeslotRepo.Setup(r => r.GetByIdAsync(3)).Returns(timeslot);
             var controller = new TimeslotsController(mocktimeslotRepo.Object);
 
             //act
-            ViewResult result = await controller.Details(3) as ViewResult;
-
+            var result = await controller.Details(3);
+            ViewResult viewResult = result as ViewResult;
             //assert
             Assert.IsNotNull(result);
-            Assert.IsNotNull(result.Model);
-            Assert.IsInstanceOfType(result.Model, typeof(Timeslot));
-            Assert.AreEqual(9, ((Timeslot)result.Model).Id);
+            //Assert.IsNotNull(viewResult.Model);
+            //Assert.IsInstanceOfType(viewResult.Model, typeof(Timeslot));
+            //Assert.AreEqual(9, ((Timeslot)viewResult.Model).Id);
+
         }
 
-        
+        private Task<List<Room>> roomlist = Task.FromResult(new List<Room>());
+        [TestMethod]
+        public async Task roomController_Index_Return_Rooms()
+        {
+            var mockroomrepo = new Mock<IRoomRepository>();
+            mockroomrepo.Setup(r => r.GetAllAsync()).Returns(roomlist);
+
+            ILogger<RoomsController> logger = new Logger<RoomsController>(new NullLoggerFactory());
+
+            var controller = new RoomsController(mockroomrepo.Object, logger);
+
+            var result = await controller.Index();
+            ViewResult viewResult = result as ViewResult;
+            //assert
+            Assert.IsNotNull(result);
+            //Assert.IsNotNull(viewResult.Model);
+            //Assert.IsInstanceOfType(viewResult.Model, typeof(Timeslot));
+            //Assert.AreEqual(9, ((Timeslot)viewResult.Model).Id);
+
+        }
+
+
     }
 }
